ce022b35a4cc70a3f1053c141acf129faeeedbbc
omjinlts <omjin7g@gmail.com>
2022-04-15 00:00:51 +0900

    :sparkles: [FEAT] : login basic implementation
    로그인 API와 연결함. 테스트를 위해 10.0.2.2(안드로이드 에뮬레이터에서의 localhost):8080과 연결해둠. 백엔드의 응답 확인까지 완료

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -1,11 +1,39 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import axios from 'axios';
+import React, {useState} from 'react';
 import {Text, Image, TouchableOpacity, TextInput, View} from 'react-native';
 import Style from './Style/Style';
 
 let imagePath = require('./images/푸앙_기본형.png');
 
 const LoginScreen = () => {
+  const [id, setId] = useState<string>(null);
+  const [password, setPassword] = useState<string>(null);
+  const login = () => {
+    var axios = require('axios');
+    var data = JSON.stringify({
+      id: id,
+      password: password,
+    });
+
+    var config = {
+      method: 'post',
+      url: 'http://10.0.2.2:8080/member/login',
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: 'JSESSIONID=A2EA07907A68294BC6A5BB472604429D',
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
     <View style={Style.container}>
       <Text style={Style.welcomeTitle}>WELCOME</Text>
@@ -29,15 +57,23 @@ const LoginScreen = () => {
       <Text style={Style.textStyle}> 아이디</Text>
       <TextInput
         style={Style.boxStyle}
-        placeholder={'아이디를 입력하세요'}></TextInput>
+        placeholder={'아이디를 입력하세요'}
+        autoCapitalize="none"
+        onChangeText={(text: string) => {
+          setId(id => text);
+        }}></TextInput>
       <Text style={Style.textStyle}> 비밀번호</Text>
       <TextInput
         style={Style.boxStyle}
         placeholder={'비밀번호를 입력하세요'}
         secureTextEntry={true}
+        textContentType="password"
+        onChangeText={(text: string) => {
+          setPassword(password => text);
+        }}
       />
       <View style={Style.center}>
-        <TouchableOpacity style={Style.buttonStyle}>
+        <TouchableOpacity style={Style.buttonStyle} onPress={login}>
           <Text
             style={[
               {
