3eee8f03681803c539e3b25e32ec71060e88d990
omjinlts <omjin7g@gmail.com>
2022-05-29 11:22:39 +0900

    :sparkles: [FEAT] bottom box naming

--- components/BottomBox.tsx
+++ components/BottomBox.tsx
@@ -4,15 +4,15 @@ import {View, Text} from 'react-native';
 import InitialStlye from './Style/InitialStyle';
 
 const BottomBox = () => {
-    return(
-        <View style={InitialStlye.bottomBox}>
-        <Text style={({padding: 20})}>icon</Text>
-        <Text style={({padding: 20})}>icon</Text>
-        <Text style={({padding: 20})}>icon</Text>
-        <Text style={({padding: 20})}>icon</Text>
-        <Text style={({padding: 20})}>icon</Text>
-      </View>
-    );
+  return (
+    <View style={InitialStlye.bottomBox}>
+      <Text style={{padding: 20}}>ranking</Text>
+      <Text style={{padding: 20}}>archive</Text>
+      <Text style={{padding: 20}}>board</Text>
+      <Text style={{padding: 20}}>chat</Text>
+      <Text style={{padding: 20}}>profile</Text>
+    </View>
+  );
 };
 
 export default BottomBox;
