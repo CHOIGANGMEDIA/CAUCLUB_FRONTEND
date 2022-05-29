c3f17899f282e26c132771deb330d4e5f1e6e8ca
omjinlts <omjin7g@gmail.com>
2022-05-29 22:09:38 +0900

    :construction: profile api linking

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -4,20 +4,26 @@ import {View, Text, TouchableHighlight} from 'react-native';
 import ProfileStyle from '../Style/ProfileStyle';
 
 const Profile = () => {
-    return(
-        <TouchableHighlight style={({borderWidth: 1, borderColor: 'black', margin: 5})}>
-          <View style={({flexDirection: 'row', justifyContent: 'space-between'})}>
-            <View style={ProfileStyle.profile}>
-                <></>
-                {/* 프로필 사진 */}
-            </View>
-            <View style={({flexDirection: 'row', width: '75%', justifyContent: 'space-between'})}>
-              <Text style={ProfileStyle.clubName}>동아리명</Text>
-              <Text style={ProfileStyle.classifyClub}>동아리분류</Text>
-            </View>
-            </View>
-        </TouchableHighlight>
-    );
+  return (
+    <TouchableHighlight
+      style={{borderWidth: 1, borderColor: 'black', margin: 5}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <View style={ProfileStyle.profile}>
+          <></>
+          {/* 프로필 사진 */}
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '75%',
+            justifyContent: 'space-between',
+          }}>
+          <Text style={ProfileStyle.clubName}>동아리명</Text>
+          <Text style={ProfileStyle.classifyClub}>동아리분류</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
 };
 
 export default Profile;
--- components/Profile/ProfileList.tsx
+++ components/Profile/ProfileList.tsx
@@ -1,26 +1,45 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {View, Text, TouchableHighlight} from 'react-native';
+import {View, Text, TouchableHighlight, ScrollView} from 'react-native';
 import InitialStlye from '../Style/InitialStyle';
 import ProfileStyle from '../Style/ProfileStyle';
 import Profile from './Profile';
-import BottomBox from '../BottomBox';
+import AsyncStorage from '@react-native-community/async-storage';
+import {customAxios} from '../../src/axiosModule/customAxios';
 
 const ProfileList = () => {
+  const [id, setId] = useState<string>('');
+  const [clubs, setClubs] = useState<number[]>();
+
+  useEffect(() => {
+    AsyncStorage.getItem('loggedId', (err, result) => {
+      if (result) setId(result);
+    }).then(() => console.log(id));
+    customAxios
+      .get(`/${id}/club`)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
-      <View style={InitialStlye.titleBox}>
-        <Text style={InitialStlye.title}>CAUCLUB</Text>
-      </View>
       <View style={ProfileStyle.topBox}>
         <Text style={InitialStlye.boardTitle}>내 동아리</Text>
         <TouchableHighlight style={ProfileStyle.newClubView}>
-            <Text style={({color: 'white', fontSize: 12, fontWeight: '900'})}>다른 동아리 보기</Text>
+          <Text style={{color: 'white', fontSize: 12, fontWeight: '900'}}>
+            다른 동아리 보기
+          </Text>
         </TouchableHighlight>
       </View>
-      <View style={{width:'100%',borderBottomWidth:0.5,borderColor:'#444'}} />
-      <KeyboardAwareScrollView>
+      <View
+        style={{width: '100%', borderBottomWidth: 0.5, borderColor: '#444'}}
+      />
+      <ScrollView>
         <Profile />
         <Profile />
         <Profile />
@@ -29,8 +48,11 @@ const ProfileList = () => {
         <Profile />
         <Profile />
         <Profile />
-      </KeyboardAwareScrollView>
-      <BottomBox />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+      </ScrollView>
     </>
   );
 };
