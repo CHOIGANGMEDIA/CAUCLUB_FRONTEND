041ae47dfa20c64496cae4eedefd54a60d22f14d
omjinlts <omjin7g@gmail.com>
2022-05-31 04:28:02 +0900

    :zap: [IMPROVE] ios-android view

--- components/MyPage.tsx
+++ components/MyPage.tsx
@@ -8,6 +8,7 @@ import {
   TextInput,
   TouchableOpacity,
   Alert,
+  SafeAreaView,
 } from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -74,65 +75,66 @@ const MyPage = () => {
   };
 
   return (
-    <KeyboardAwareScrollView style={Style.container}>
-      <View style={[{marginTop: 10}]}>
-        <Text style={Style.appTitle}>My Page</Text>
-        <View style={Style.imageContainer}>
-          <Image style={Style.image} source={imagePath} />
-          <Text
-            style={[
-              {
-                color: 'black',
-                fontSize: 20,
-                fontWeight: '900',
-                right: 20,
-                fontStyle: 'italic',
-              },
-            ]}>
-            {' '}
-            뭐라할까{' '}
-          </Text>
-        </View>
-        <View style={[{margin: 10}]} />
-        <Text style={Style.textStyle}>비밀번호 재설정</Text>
-        <TextInput
-          style={Style.boxStyle}
-          placeholder={'비밀번호를 유지하시려면 비워두세요'}></TextInput>
-        <Text style={Style.textStyle}>비밀번호 재설정 확인</Text>
-        <TextInput
-          style={Style.boxStyle}
-          placeholder={'비밀번호를 한 번 더 입력하세요'}></TextInput>
-        <Text style={Style.textStyle}>이름</Text>
-        <TextInput
-          style={Style.boxStyle}
-          onChangeText={text => setName(text)}
-          value={name}></TextInput>
-        <Text style={Style.textStyle}>이메일</Text>
-        {emailMsg ? <Text style={Style.warnSubStyle}>{emailMsg}</Text> : null}
-        <TextInput
-          style={Style.boxStyle}
-          onChangeText={text => emailChanged(text)}
-          value={email}></TextInput>
-
-        <View style={[{margin: 30}]} />
-        <View style={Style.center}>
-          <TouchableOpacity style={Style.buttonStyle} onPress={modifyPressed}>
+    <SafeAreaView style={Style.container}>
+      <KeyboardAwareScrollView style={Style.container}>
+        <View style={[{marginTop: 10}]}>
+          <Text style={Style.appTitle}>My Page</Text>
+          <View style={Style.imageContainer}>
+            <Image style={Style.image} source={imagePath} />
             <Text
               style={[
                 {
-                  color: 'white',
-                  textAlign: 'center',
+                  color: 'black',
+                  fontSize: 20,
                   fontWeight: '900',
-                  fontSize: 15,
+                  right: 20,
+                  fontStyle: 'italic',
                 },
               ]}>
-              수정하기
+              {' '}
+              뭐라할까{' '}
             </Text>
-          </TouchableOpacity>
+          </View>
+          <View style={[{margin: 10}]} />
+          <Text style={Style.textStyle}>비밀번호 재설정</Text>
+          <TextInput
+            style={Style.boxStyle}
+            placeholder={'비밀번호를 유지하시려면 비워두세요'}></TextInput>
+          <Text style={Style.textStyle}>비밀번호 재설정 확인</Text>
+          <TextInput
+            style={Style.boxStyle}
+            placeholder={'비밀번호를 한 번 더 입력하세요'}></TextInput>
+          <Text style={Style.textStyle}>이름</Text>
+          <TextInput
+            style={Style.boxStyle}
+            onChangeText={text => setName(text)}
+            value={name}></TextInput>
+          <Text style={Style.textStyle}>이메일</Text>
+          {emailMsg ? <Text style={Style.warnSubStyle}>{emailMsg}</Text> : null}
+          <TextInput
+            style={Style.boxStyle}
+            onChangeText={text => emailChanged(text)}
+            value={email}></TextInput>
+
+          <View style={[{margin: 10}]} />
+          <View style={Style.center}>
+            <TouchableOpacity style={Style.buttonStyle} onPress={modifyPressed}>
+              <Text
+                style={[
+                  {
+                    color: 'white',
+                    textAlign: 'center',
+                    fontWeight: '900',
+                    fontSize: 15,
+                  },
+                ]}>
+                수정하기
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={[{margin: 30}]} />
-      </View>
-    </KeyboardAwareScrollView>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
 };
 
