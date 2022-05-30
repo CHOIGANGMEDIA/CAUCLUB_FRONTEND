086f4b6b698b54234df0dee3c8a0435007103924
omjinlts <omjin7g@gmail.com>
2022-05-30 23:40:37 +0900

    :twisted_rightwards_arrows: [MERGE]

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import BoardList from './components/BoardList';
 import BoardModify from './components/BoardModify';
 import BoardNew from './components/BoardNew';
@@ -8,18 +8,35 @@ import BoardScreen from './components/BoardScreen';
 import LoginScreen from './components/LoginScreen';
 import MyPage from './components/MyPage';
 import BoardNavigator from './components/navigation/BoardNavigator';
+import ProfileList from './components/Profile/ProfileList';
+import Profile from './components/Profile/Profile';
 import RegisterScreen from './components/RegisterScreen';
 import SearchID from './components/SearchID';
 import SearchPW from './components/SearchPW';
+import BottomBox from './components/BottomBox/BottomBox';
+import Style from './components/Style/Style';
+import TopBox from './components/TopBox';
+import ProfilePageP from './components/Profile/ProfilePageP';
+import ProfilePageMtrue from './components/Profile/ProfilePageMtrue';
+import RecommendProfile from './components/Profile/RecommendProfile';
+import RecommendClubList from './components/Profile/RecommendClubList';
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <BoardNavigator />
-      </NavigationContainer>
+    // <SafeAreaProvider>
+    //   <NavigationContainer>
+    //     <BoardNavigator />
+    //   </NavigationContainer>
+    // </SafeAreaProvider>
+    <SafeAreaView style={{flex: 1}}>
       {/* <RegisterScreen /> */}
-    </SafeAreaProvider>
+      {/* <TopBox />
+      <ProfilePageP />
+      <BottomBox /> */}
+      {/* <LoginScreen /> */}
+      <MyPage />
+    </SafeAreaView>
   );
 };
+
 export default App;
