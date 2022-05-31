7dc247e0570f81d5b5cf1abbe4b6b054fa796e08
omjinLTS <omjin7g@gmail.com>
2022-06-01 08:09:06 +0900

    :sparkles: Ranking

--- components/Ranking/RankProfile.tsx
+++ components/Ranking/RankProfile.tsx
@@ -1,31 +1,51 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TouchableHighlight, Image, ScrollView} from 'react-native';
 import RankingStyle from '../Style/RankingStyle';
+import type {Club} from '../Profile/Club';
+import {customAxios} from '../../src/axiosModule/customAxios';
 
 let imagePath = require('../images/푸앙_응원.png');
+
+type RankProfileProps = {
+  rank: number;
+  clubId: number;
+};
+
+export type {RankProfileProps};
 
 // TODO API link
 // TODO 터치시 동아리상세로
 
-const RankProfile = () => {
+const RankProfile = ({rank, clubId}: RankProfileProps) => {
+  const [club, setClub] = useState<Club>();
+
+  customAxios
+    .get(`/club/${clubId}`)
+    .then(response => {
+      setClub(response.data);
+    })
+    .catch(error => console.log(error));
+
   return (
     <TouchableHighlight style={RankingStyle.profile}>
       <View style={{flexDirection: 'row'}}>
         <View style={RankingStyle.rankNumber}>
-          <Text style={RankingStyle.rankFont}>1등</Text>
+          <Text style={RankingStyle.rankFont}>{rank}등</Text>
         </View>
         <Image style={RankingStyle.profileImage} source={imagePath} />
         <View style={RankingStyle.clubName}>
           <ScrollView horizontal={true}>
-            <Text style={RankingStyle.clubNameFont}>동아리명</Text>
+            <Text style={RankingStyle.clubNameFont}>{club?.name}</Text>
           </ScrollView>
         </View>
         <View style={RankingStyle.classifiedClub}>
-          <Text style={RankingStyle.classifiedClubFont}>예체능</Text>
+          <Text style={RankingStyle.classifiedClubFont}>
+            {club?.type.substring(0, club?.type.length - 3)}
+          </Text>
         </View>
         <View style={RankingStyle.scoreView}>
-          <Text style={RankingStyle.score}>40점</Text>
+          <Text style={RankingStyle.score}>{club?.score}점</Text>
         </View>
       </View>
     </TouchableHighlight>
--- components/Ranking/Ranking.tsx
+++ components/Ranking/Ranking.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TouchableHighlight, Image, ScrollView} from 'react-native';
+import {customAxios} from '../../src/axiosModule/customAxios';
 import {NavigationHeader} from '../navigation/NavigationHeader';
 import {SafeAreaView} from '../navigation/SafeAreaView';
 import InitialStlye from '../Style/InitialStyle';
@@ -9,8 +11,31 @@ import RankProfile from './RankProfile';
 
 let imagePath = require('../images/푸앙_응원.png');
 
+// 동아리 type
+// 0 : 전체
+// 1 : 학술
+// 2 : 예체능
+// 3 : 기타
+
 const Ranking = () => {
-  // TODO api
+  const [selectedType, setSelectedtype] = useState<number>(0);
+  const isFocused = useIsFocused();
+  const [clubList, setClubList] = useState<number[]>([]);
+
+  // initial Loading
+  useEffect(() => {
+    if (isFocused) {
+      customAxios.get(`/rank/${selectedType}`).then(async response => {
+        setClubList(response.data);
+      });
+    }
+    return setClubList([]);
+  }, [isFocused, selectedType]);
+
+  const rankProfiles = clubList?.map((clubId, idx) => {
+    return <RankProfile key={clubId} rank={idx + 1} clubId={clubId} />;
+  });
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <NavigationHeader />
@@ -40,16 +65,40 @@ const Ranking = () => {
       <View style={{height: '87%', width: '90%', margin: '5%'}}>
         <View style={{height: 40, width: '100%', flexDirection: 'row'}}>
           {/* TODO 각 랭킹 불러오기 useEffect 사용하면 될듯 */}
-          <TouchableHighlight style={RankingStyle.tappedButton}>
+          <TouchableHighlight
+            style={
+              selectedType == 0
+                ? RankingStyle.tappedButton
+                : RankingStyle.untappedButton
+            }
+            onPress={() => setSelectedtype(0)}>
             <Text style={RankingStyle.fontStyle}>전체</Text>
           </TouchableHighlight>
-          <TouchableHighlight style={RankingStyle.untappedButton}>
+          <TouchableHighlight
+            style={
+              selectedType == 1
+                ? RankingStyle.tappedButton
+                : RankingStyle.untappedButton
+            }
+            onPress={() => setSelectedtype(1)}>
             <Text style={RankingStyle.fontStyle}>학술</Text>
           </TouchableHighlight>
-          <TouchableHighlight style={RankingStyle.untappedButton}>
+          <TouchableHighlight
+            style={
+              selectedType == 2
+                ? RankingStyle.tappedButton
+                : RankingStyle.untappedButton
+            }
+            onPress={() => setSelectedtype(2)}>
             <Text style={RankingStyle.fontStyle}>예체능</Text>
           </TouchableHighlight>
-          <TouchableHighlight style={RankingStyle.untappedButton}>
+          <TouchableHighlight
+            style={
+              selectedType == 3
+                ? RankingStyle.tappedButton
+                : RankingStyle.untappedButton
+            }
+            onPress={() => setSelectedtype(3)}>
             <Text style={RankingStyle.fontStyle}>기타</Text>
           </TouchableHighlight>
         </View>
@@ -62,8 +111,7 @@ const Ranking = () => {
             borderColor: '#143365',
           }}>
           {/* TODO ranking iteration*/}
-          <RankProfile />
-          <RankProfile />
+          {rankProfiles}
         </ScrollView>
       </View>
     </SafeAreaView>
