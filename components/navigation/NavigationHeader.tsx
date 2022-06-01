750472871ce1f1c14aafe983597206ae5fb24f13
omjinLTS <omjin7g@gmail.com>
2022-06-01 12:13:44 +0900

    :wrench: [MOD]

--- components/navigation/NavigationHeader.tsx
+++ components/navigation/NavigationHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import type {FC, ReactNode} from 'react';
 import {
   StatusBar,
@@ -9,27 +9,49 @@ import {
   ViewStyle,
 } from 'react-native';
 import InitialStlye from '../Style/InitialStyle';
+import {useNavigation} from '@react-navigation/native';
+import {MaterialCommunityIcon as Icon} from './MaterialCommunityIcon';
 
 export type NavigationHeaderProps = {
   title?: string;
-  Left?: () => ReactNode;
-  Right?: () => ReactNode;
+  Left?: boolean;
   viewStyle?: StyleProp<ViewStyle>;
   titleStyle?: StyleProp<ViewStyle>;
 };
 
+// TODO 위에도 파랑으로 채우기 - 부모 컴포넌트에서의 문제일 가능성 큼(네비게이터)
+// TODO css
+
 export const NavigationHeader: FC<NavigationHeaderProps> = ({
   title,
-  Left,
-  Right,
+  Left = false,
   viewStyle,
-  titleStyle,
 }) => {
+  const navigation = useNavigation<any>();
+  const goBack = useCallback(() => {
+    navigation.canGoBack() && navigation.goBack();
+    console.log('back pressed');
+  }, []);
   return (
     <View style={[styles.view, viewStyle]}>
-      <View style={{flex: 1}}>{Left && Left()}</View>
+      <View style={{flex: 1}}>
+        {Left ? (
+          <Icon
+            name="chevron-left"
+            size={30}
+            onPress={goBack}
+            style={{backgroundColor: 'transparent'}}
+          />
+        ) : null}
+      </View>
       <Text style={[InitialStlye.title]}>CAUCLUB</Text>
-      <View style={{flex: 1}}>{Right && Right()}</View>
+      <View style={{flex: 1}}>
+        <Icon
+          name="account-outline"
+          size={24}
+          onPress={() => navigation.navigate('MyPage', {})}
+        />
+      </View>
     </View>
   );
 };
